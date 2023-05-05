a83d637505339fbb9e096b58156bc39edbee7824
bevans-o <81346899+bevans-o@users.noreply.github.com>
2023-05-05 21:51:35 +1000

    matter.js running in new tab

--- src/newTab.jsx
+++ src/newTab.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
 import './main.css';
+import Matter from "matter-js";
 
 function NewTab() {
     return (
-        <div>
-            <h1>Fishbowl</h1>
-            <p>pee pee poo poo</p>
-        </div>
+        <>
+            <div>
+                <h1>Fishbowl</h1>
+                <p>pee pee poo poo</p>
+            </div>
+
+            <script type="text/javascript" src="./physics.js"></script>
+        </>
+        
     );
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,11 @@ const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+    devtool: 'source-map', // for matter.js, https://github.com/liabru/matter-js/issues/1001
     mode: 'production',
     entry: {
         newTab: './src/newTab.jsx',
+        physics: './src/physics.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
