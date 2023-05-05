133f23f40f0f8f64b526a4fa844a48277a0a1131
bevans-o <81346899+bevans-o@users.noreply.github.com>
2023-05-06 03:07:30 +1000

    grubby little mouth

--- frontend/src/physics.js
+++ frontend/src/physics.js
@@ -10,6 +10,23 @@ function makeFish(type, length, height, x, y) {
 
     var fish = Composite.create({ label: type });
 
+    var mouth = Bodies.rectangle(x - (3 * length / 8), y + height / 4, height / 2, height / 5, { 
+        collisionFilter: {
+            group: group
+        },
+        chamfer: {
+            radius: height * 0.5
+        },
+        restitution: 0.4,
+        friction: 0.8,
+        render: {
+            sprite:  {
+                texture: './fish/' + type + '/mouth.png',
+                xScale: 0.5,
+                yScale: 0.5
+            }
+    }});
+
     var head = Bodies.rectangle(x - length / 4, y, length / 3, height, { 
         collisionFilter: {
             group: group
@@ -17,6 +34,7 @@ function makeFish(type, length, height, x, y) {
         chamfer: {
             radius: height * 0.5
         },
+        restitution: 0.4,
         friction: 0.8,
         render: {
             sprite:  {
@@ -33,6 +51,7 @@ function makeFish(type, length, height, x, y) {
         chamfer: {
             radius: height * 0.5
         },
+        restitution: 0.4,
         density: 0.0002,
         render: {
             sprite:  {
@@ -42,10 +61,30 @@ function makeFish(type, length, height, x, y) {
             }
     }});
 
+    var tail = Bodies.rectangle(x + length / 4, y - 5, length / 3, height, { 
+        collisionFilter: {
+            group: group
+        },
+        chamfer: {
+            radius: height * 0.5
+        },
+        restitution: 0.4,
+        density: 0.0002,
+        render: {
+            sprite:  {
+                texture: './fish/' + type + '/tail.png',
+                xScale: 0.5,
+                yScale: 0.5
+            }
+    }});
+
     var eye = Bodies.circle(x, y, height / 4, { 
         collisionFilter: {
             group: group
         },
+        restitution: 0.4,
+        friction: 0.8,
+        density: 0.0002,
         render: {
             sprite:  {
                 texture: './fish/' + type + '/eye.png',
@@ -60,24 +99,53 @@ function makeFish(type, length, height, x, y) {
     var headBody = Constraint.create({
         bodyA: head,
         bodyB: body,
-        pointA: { x: length / 12, y: 0 },
-        pointB: { x: -length / 12, y: 0 },
-        stiffness: 0,
+        pointA: { x: length / 16, y: 0 },
+        pointB: { x: -length / 16, y: 0 },
+        stiffness: 0.8,
+        damping: 0.1,
         length: 0
     });
                     
     var headEye = Constraint.create({
         bodyA: head,
         bodyB: eye,
-        pointA: { x: -30, y: -10 },
-        stiffness: 0.5,
+        pointA: { x: -30, y: -5 },
+        stiffness: 1,
+        length: 0
+    });
+
+    var headMouthA = Constraint.create({
+        bodyA: head,
+        bodyB: mouth,
+        pointA: { x: -30, y: 8 },
+        pointB: { x: 10, y: 0 },
+        stiffness: 1,
+        length: 0
+    });
+
+    var headMouthB = Constraint.create({
+        bodyA: head,
+        bodyB: mouth,
+        pointA: { x: -25, y: 13 },
+        pointB: { x: 15, y: 5 },
+        stiffness: 0.2,
+        length: 3
+    });
+
+    var bodyTail = Constraint.create({
+        bodyA: body,
+        bodyB: tail,
+        pointA: { x: length / 16, y: 0 },
+        pointB: { x: -length / 16, y: 2 },
+        stiffness: 0.8,
+        damping: 0.1,
         length: 0
     });
     
     Composite.addBody(fish, head);
     Composite.addBody(fish, body);
-    // Composite.addBody(fish, tail);
-    // Composite.addBody(fish, mouth);
+    Composite.addBody(fish, tail);
+    Composite.addBody(fish, mouth);
     Composite.addBody(fish, eye);
     // Composite.addBody(fish, pectoral);
     // Composite.addBody(fish, pelvicA);
@@ -89,6 +157,9 @@ function makeFish(type, length, height, x, y) {
     // Composite.addBody(fish, dorsalB);
     Composite.addConstraint(fish, headBody);
     Composite.addConstraint(fish, headEye);
+    Composite.addConstraint(fish, headMouthA);
+    Composite.addConstraint(fish, headMouthB);
+    Composite.addConstraint(fish, bodyTail);
 
     return fish;
 };
