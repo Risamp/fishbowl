002b9c2133d66c08609178ea972742e11a53366d
Risa <risapais@gmail.com>
2023-05-07 07:28:19 +1000

    update database connection

--- Fish.py
+++
@@ -1,7 +0,0 @@
-class Fish(object):
-
-    def __init__(self, name, scientificName, status, populationDecreasing):
-        self.name = name
-        self.scientificName = scientificName
-        self.status = status
-        self.populationDecreasing = populationDecreasing
